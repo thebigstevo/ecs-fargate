7d6a4da1335347442702f78d600575c7bd06ac66
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-03-31 22:11:35 +0000

    updated security group file

---
+++ security.tf
@@ -0,0 +1,42 @@
+# ALB security Group: Edit to restrict access to the application
+resource "aws_security_group" "alb_sg" {
+    name = "app-loadbalancer-security-group"
+    description = "controls access to the ALB"
+    vpc_id = aws_vpc.ecs-main.id
+
+    ingress {
+        protocol = "tcp"
+        from_port = var.app_port
+        to_port = var.app_port
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        protocol = "-1"
+        from_port = 0
+        to_port = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+
+
+# Traffic to the ECS cluster should only come from the ALB
+resource "aws_security_group" "ecs_tasks" {
+    name = "app-ecs-tasks-security-group"
+    description = "allow inbound access from the ALB only"
+    vpc_id = aws_vpc.ecs-main.id
+
+    ingress {
+        protocol = "tcp"
+        from_port = var.app_port
+        to_port = var.app_port
+        security_groups = [aws_security_group.alb_sg.name]
+    }
+
+    egress {
+        protocol = "-1"
+        from_port = 0
+        to_port = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
