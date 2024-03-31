4bcd0d0fb294d53bf15022d1f5ecffe239bda734
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-03-31 20:05:16 +0000

    changed vpc name

--- network.tf
+++ network.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "ecs-main" {
     cidr_block = "172.17.0.0/16"
 }
 
@@ -11,28 +11,28 @@ data "aws_availability_zones" "available" {
 # Create var.az_count private subnets, each in a different AZ
 resource "aws_subnet" "private" {
     count             = var.az_count
-    cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+    cidr_block        = cidrsubnet(aws_vpc.ecs-main.cidr_block, 8, count.index)
     availability_zone = data.aws_availability_zones.available.names[count.index]
-    vpc_id            = aws_vpc.main.id
+    vpc_id            = aws_vpc.ecs-main.id
 }
 
 # Create var.az_count public subnets, each in a different AZ
 resource "aws_subnet" "public" {
     count                   = var.az_count
-    cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, var.az_count + count.index)
+    cidr_block              = cidrsubnet(aws_vpc.ecs-main.cidr_block, 8, var.az_count + count.index)
     availability_zone       = data.aws_availability_zones.available.names[count.index]
-    vpc_id                  = aws_vpc.main.id
+    vpc_id                  = aws_vpc.ecs-main.id
     map_public_ip_on_launch = true
 }
 
 # Internet Gateway for the public subnet
 resource "aws_internet_gateway" "gw" {
-    vpc_id = aws_vpc.main.id
+    vpc_id = aws_vpc.ecs-main.id
 }
 
 # Route the public subnet traffic through the IGW
 resource "aws_route" "internet_access" {
-    route_table_id         = aws_vpc.main.main_route_table_id
+    route_table_id         = aws_vpc.ecs-main.main_route_table_id
     destination_cidr_block = "0.0.0.0/0"
     gateway_id             = aws_internet_gateway.gw.id
 }
@@ -53,7 +53,7 @@ resource "aws_nat_gateway" "gw" {
 # Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
 resource "aws_route_table" "private" {
     count  = var.az_count
-    vpc_id = aws_vpc.main.id
+    vpc_id = aws_vpc.ecs-main.id
 
     route {
         cidr_block     = "0.0.0.0/0"
