92b91bbea923a2afbd019f9869c7eac84593e136
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-04-01 05:08:41 +0000

    changed alb port in security group

--- security.tf
+++ security.tf
@@ -6,8 +6,8 @@ resource "aws_security_group" "alb_sg" {
 
     ingress {
         protocol = "tcp"
-        from_port = var.app_port
-        to_port = var.app_port
+        from_port = 80
+        to_port = 80
         cidr_blocks = ["0.0.0.0/0"]
     }
     egress {
