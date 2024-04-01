52396e1d8eab91ceae5d7c61fbd9094f9c3210b8
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-04-01 05:37:51 +0000

    changed image to ollama

--- variables.tf
+++ variables.tf
@@ -30,7 +30,7 @@ variable "az_count" {
 
 variable "app_image" {
     description = "Docker image to run in the ECS cluster"
-    default = "bradfordhamilton/crystal_blockchain:latest"
+    default = "ollama/ollama:latest"
 }
 
 variable "app_port" {
