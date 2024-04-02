30eb84d8149b8074d866ca5d39d2151aa9f378a2
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-04-02 10:56:14 +0000

    changes in formatting

--- iam.tf
+++ iam.tf
@@ -19,7 +19,7 @@ EOF
 }
 resource "aws_iam_role" "ecs_task_role" {
   name = "role-name-task"
- 
+
   assume_role_policy = <<EOF
 {
  "Version": "2012-10-17",
@@ -36,21 +36,21 @@ resource "aws_iam_role" "ecs_task_role" {
 }
 EOF
 }
- 
+
 resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "task_s3" {
-  role       = "${aws_iam_role.ecs_task_role.name}"
+  role       = aws_iam_role.ecs_task_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
 data "aws_iam_policy_document" "ecs_auto_scale_role" {
   version = "2012-10-17"
   statement {
-    effect = "Allow"
+    effect  = "Allow"
     actions = ["sts:AssumeRole"]
 
     principals {
