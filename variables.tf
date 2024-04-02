6f38069186b545cbb405c6a3d5b662c513a007ba
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-04-02 10:58:43 +0000

    updated variables with appropriate names

--- variables.tf
+++ variables.tf
@@ -1,47 +1,64 @@
 # variables.tf
 
 variable "aws_access_key" {
-    description = "The IAM public access key"
+  description = "The IAM public access key"
 }
 
 variable "aws_secret_key" {
-    description = "IAM secret access key"
+  description = "IAM secret access key"
 }
 
 variable "aws_region" {
-    description = "The AWS region things are created in"
-    default = "eu-west-2"
+  description = "The AWS region things are created in"
+  default     = "eu-west-2"
 }
 
 variable "ec2_task_execution_role_name" {
-    description = "ECS task execution role name"
-    default = "myEcsTaskExecutionRole"
+  description = "ECS task execution role name"
+  default     = "myEcsTaskExecutionRole"
 }
 
 variable "ecs_auto_scale_role_name" {
-    description = "ECS auto scale role name"
-    default = "myEcsAutoScaleRole"
+  description = "ECS auto scale role name"
+  default     = "myEcsAutoScaleRole"
 }
 
 variable "az_count" {
-    description = "Number of AZs to cover in a given region"
-    default = "2"
+  description = "Number of AZs to cover in a given region"
+  default     = "2"
 }
 
-variable "app_image" {
-    description = "Docker image to run in the ECS cluster"
-    default = "ollama/ollama:latest"
+variable "ollama_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "ollama/ollama:latest"
+}
+variable "ollama_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 11434
+
 }
 
-variable "app_port" {
-    description = "Port exposed by the docker image to redirect traffic to"
-    default = 11434
+variable "webui_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "ghcr.io/open-webui/open-webui:git-26a187f"
+}
+
+variable "webui_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 8080
 
 }
 
+variable "ollama_backend_container" {
+  default = "ollama_backend_container"
+}
+
+variable "webui_container" {
+  default = "webui_container"
+}
 variable "app_count" {
-    description = "Number of docker containers to run"
-    default = 1
+  description = "Number of docker containers to run"
+  default     = 1
 }
 
 variable "health_check_path" {
@@ -49,11 +66,11 @@ variable "health_check_path" {
 }
 
 variable "fargate_cpu" {
-    description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-    default = "1024"
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "1024"
 }
 
 variable "fargate_memory" {
-    description = "Fargate instance memory to provision (in MiB)"
-    default = "2048"
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "2048"
 }
