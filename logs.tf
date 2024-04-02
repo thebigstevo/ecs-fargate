1cd364dd28f4579243028e9045b400302d52f1ec
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-04-02 12:26:41 +0000

    updated loggroup

--- logs.tf
+++ logs.tf
@@ -1,16 +1,31 @@
 # logs.tf
 
 # Set up CloudWatch group and log stream and retain logs for 30 days
-resource "aws_cloudwatch_log_group" "cb_log_group" {
-  name              = "/ecs/cb-app"
+resource "aws_cloudwatch_log_group" "ollama_log_group" {
+  name              = "/ecs/ollama"
   retention_in_days = 30
 
   tags = {
-    Name = "cb-log-group"
+    Name = "ollama"
   }
 }
 
-resource "aws_cloudwatch_log_stream" "cb_log_stream" {
-  name           = "cb-log-stream"
-  log_group_name = aws_cloudwatch_log_group.cb_log_group.name
+resource "aws_cloudwatch_log_stream" "ollama_log_stream" {
+  name           = "ollama-log-stream"
+  log_group_name = aws_cloudwatch_log_group.ollama_log_group.name
+}
+
+# Set up CloudWatch group and log stream and retain logs for 30 days
+resource "aws_cloudwatch_log_group" "webui_log_group" {
+  name              = "/ecs/webui"
+  retention_in_days = 30
+
+  tags = {
+    Name = "webui"
+  }
+}
+
+resource "aws_cloudwatch_log_stream" "webui_log_stream" {
+  name           = "webui-stream"
+  log_group_name = aws_cloudwatch_log_group.ollama_log_group.name
 }
