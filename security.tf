5eb1bf86a36d2d612177d594fc2bbdab92ff6098
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-04-02 10:58:09 +0000

    updated security group with appropriate resource names

--- security.tf
+++ security.tf
@@ -1,43 +1,43 @@
 # ALB security Group: Edit to restrict access to the application
 resource "aws_security_group" "alb_sg" {
-    name = "app-loadbalancer-security-group"
-    description = "controls access to the ALB"
-    vpc_id = aws_vpc.ecs-main.id
+  name        = "app-loadbalancer-security-group"
+  description = "controls access to the ALB"
+  vpc_id      = aws_vpc.ecs-main.id
 
-    ingress {
-        protocol = "tcp"
-        from_port = 80
-        to_port = 80
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        protocol = "-1"
-        from_port = 0
-        to_port = 0
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
 
 # Traffic to the ECS cluster should only come from the ALB
 resource "aws_security_group" "ecs_tasks" {
-    name = "app-ecs-tasks-security-group"
-    description = "allow inbound access from the ALB only"
-    vpc_id = aws_vpc.ecs-main.id
+  name        = "app-ecs-tasks-security-group"
+  description = "allow inbound access from the ALB only"
+  vpc_id      = aws_vpc.ecs-main.id
 
-    ingress {
-        protocol = "tcp"
-        from_port = var.app_port
-        to_port = var.app_port
-        security_groups = [aws_security_group.alb_sg.id]
-    }
+  ingress {
+    protocol        = "tcp"
+    from_port       = var.webui_port
+    to_port         = var.webui_port
+    security_groups = [aws_security_group.alb_sg.id]
+  }
 
-    egress {
-        protocol = "-1"
-        from_port = 0
-        to_port = 0
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    depends_on = [ aws_security_group.alb_sg ]
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  depends_on = [aws_security_group.alb_sg]
 }
